d5b62a515f4f06ad556fec6780150f16e38e16bc
anniechakraborty <the.demented.genius29@gmail.com>
2025-04-04 00:59:43 +0200

    init

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomeComponent(){
+    return (
+        <div>
+            <h1>Home Component</h1>
+            <p>Welcome to the Home Page!</p>
+        </div>
+    );
+}
+
+export default HomeComponent;
