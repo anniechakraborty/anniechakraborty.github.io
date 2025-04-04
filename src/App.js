8ed76bd8a9a40af7f263e342df2eca24dbd7d65e
anniechakraborty <the.demented.genius29@gmail.com>
2025-04-04 02:12:52 +0200

    basics addition on nabvar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // Import Bootstrap JavaScript
 
 import HomeComponent from './components/Home';
+import NavigationComponent from './components/Navigation';
 
 function App() {
   return (
       <div className="App">
+        <NavigationComponent />
         <header className="App-header">
           <HomeComponent />
         </header>
---
+++ src/components/Navigation.js
@@ -0,0 +1,40 @@
+import React from "react";
+import './../styles/Navigation.css'
+function NavigationComponent() {
+  return (
+    <nav className="navbar navbar-expand-lg container">
+      <a className="navbar-brand" href="#">
+        Annie
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        onClick={()=> {console.log('clicked!!')}}
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Link
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default NavigationComponent;
