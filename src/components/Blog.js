74c8444ea88d51b6710e010534a10fcb874a0143
anniechakraborty <the.demented.genius29@gmail.com>
2025-04-14 00:46:08 +0200

    blog content integrated

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,21 +1,46 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-export default function Blog(){
-    const [posts, setPosts] = useState([]);
-    
-    useEffect(()=>{
-        const fetchMediumPosts = async () => {
-            try {
-              const res = await axios.get('http://localhost:5000/api/medium-posts');
-              console.log(res);
-              setPosts(res.data);
-            } catch (err) {
-              console.error('Error fetching posts data:', err);
-            } finally {
-            }
-          };
-      
-          fetchMediumPosts();
-    }, [])
+import "./../styles/Blog.css";
+
+export default function Blog() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchMediumPosts = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/api/medium-posts");
+        console.log(res.data);
+        setPosts(res.data);
+      } catch (err) {
+        console.error("Error fetching posts data:", err);
+      } finally {
+      }
+    };
+
+    fetchMediumPosts();
+  }, []);
+
+  return (
+    <div className="blogContainer">
+      {posts.map((post, index) => (
+        <div key={index} className="postCard">
+          <h3>{post.title}</h3>
+          <p className="publishedDate">
+            {new Date(post.published).toLocaleDateString()}
+          </p>
+          {post.summary && (
+            <p className="summary">
+              {post.summary.length > 500
+                ? post.summary.slice(0, 500) + "..."
+                : post.summary}
+            </p>
+          )}
+          <a href={post.link} target="_blank" rel="noopener noreferrer">
+            Show More
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 }
